573ee29d373f032abaaa33dccd7dab33a80ce9bf
Fedeya <elfedeminaya@gmail.com>
2020-06-10 09:16:52 -0300

    [Feat] added theme provider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import AboutProvider from './src/context/about/AboutContext';
+import ThemeProvider from './src/context/theme/ThemeContext';
 
 export const wrapRootElement = ({ element }) => (
   <AboutProvider>
-    {element}
+    <ThemeProvider>
+      {element}
+    </ThemeProvider>
   </AboutProvider>
-)
+);
