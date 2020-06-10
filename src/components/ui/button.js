4d71fb3fbdd9ebbee9c49ca3499e6a127996ab5d
Fedeya <elfedeminaya@gmail.com>
2020-06-10 07:05:23 -0300

    [Feat] added button for links

--- src/components/ui/button.js
+++ src/components/ui/button.js
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { Link } from 'gatsby';
 
 const Button = styled.button`
   background-color: var(--orange);
@@ -18,6 +19,34 @@ const Button = styled.button`
   align-items: center;
   justify-content: center;
   transition: all 500ms ease; 
+
+  &:hover {
+    background-color: var(--dark);
+    border: 1px solid var(--orange);
+    color: var(--orange);
+  }
+`;
+
+export const ButtonLink = styled(Link)`
+  text-decoration: none;
+  background-color: var(--orange);
+  font-family: 'Roboto Slab', serif;
+  text-align: center;
+  border-radius: 10px;
+  font-weight: bold;
+  text-transform: uppercase;
+  border: none;
+  font-size: 20px;
+  width: 181px;
+  height: 64px;
+  outline: none;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  transition: all 500ms ease; 
+  color: var(--dark);
 
   &:hover {
     background-color: var(--dark);
