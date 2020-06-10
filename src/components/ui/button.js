53ef81e2567e758ca25907852fc14261f1e52089
Fedeya <elfedeminaya@gmail.com>
2020-06-10 05:01:52 -0300

    [Feat] created button ui component

---
+++ src/components/ui/button.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  background-color: var(--orange);
+  font-family: 'Roboto Slab', serif;
+  text-align: center;
+  border-radius: 10px;
+  font-weight: bold;
+  text-transform: uppercase;
+  border: 1px solid #ccc;
+  font-size: 20px;
+  width: 181px;
+  height: 64px;
+`;
+
+export default Button;
