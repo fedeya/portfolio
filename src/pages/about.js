c93521c02e8d9309217a0ce19063e2c2a08b59b3
Fedeya <elfedeminaya@gmail.com>
2020-06-10 08:30:16 -0300

    [Feat] used components in the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Layout from '../components/layout/layout';
+import AboutContent from '../components/about/about-content';
 
 function AboutPage() {
   return (
     <Layout>
-      <h1>AboutPage Works!</h1>
+      <AboutContent />
     </Layout>
   );
 }
