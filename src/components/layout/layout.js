05d0e2193c10d3eac9621d81af3cca136ca80bbf
Fedeya <elfedeminaya@gmail.com>
2020-06-16 16:57:29 -0300

    [Feat] added meta description

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -10,6 +10,7 @@ function Layout({ children }) {
     <>
       <Helmet>
         <title>Federico Minaya</title>
+        <meta name="description" content="Federico Minaya is a Frontend Developer with 16 years old"/>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
         <link href='https://unpkg.com/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet' />
